40c7201c0cf032ef50c876dbe3ede29eec8ba6be
hybrydo <hybrydo@gmail.com>
2013-05-09 01:55:17 +0200

    creando carpetas unidad 1 y 2

---
+++ Unidad 1/Ejercicios/Sem1Ejer1+IbanSanchez.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#!coding: utf-8
+
+inicio_curso = input ("Introduce el año de inicio del curso:")
+
+for z in range(1,4):
+
+    name = raw_input ("¿Tu nombre?:>>>  ")
+    year_birth = raw_input ("¿Tu año de nacimiento?:>>>  ")
+    old = int(inicio_curso) - int(year_birth)
+    print ("Tu nombre es ", name, "Este año cumples",old)
---
+++ Unidad 1/Ejercicios/Sem1Ejer2+IbanSanchez.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#!coding: utf-8
+
+inicio_num = input ("introduce un numero:")
+
+for z in range(0,9):
+
+    print(inicio_num)
+    inicio_num += z
