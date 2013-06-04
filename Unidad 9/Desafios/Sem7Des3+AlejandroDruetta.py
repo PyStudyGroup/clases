5ad600d943723b4c4a371b1654b029e7a025c5d4
Alejandro Druetta <aledruetta@gmail.com>
2013-06-04 19:02:52 -0300

    Semana 7 Desafío 3 Alejandro Druetta

---
+++ Unidad 9/Desafios/Sem7Des3+AlejandroDruetta.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def a_palabras(frase):
+
+    permitidos = "abcdefghijklmnñopqrstuvwxyz áéíóú"
+    frase_limp = ""
+    frase_min = frase.lower()
+
+    for i in frase_min:
+        if i in permitidos:
+            frase_limp += i
+
+    palabras = frase_limp.split()
+
+    return palabras
+
+def main():
+
+    diccionario = {}
+    lengua = raw_input("¿Qué idioma usaremos para la traducción?: ")
+
+    while True:
+
+        print
+        frase = raw_input( "Escriba una frase completa ('*' para terminar): " )
+
+        if frase == "*":
+            break
+        else:
+            palabras = a_palabras(frase)
+
+        print
+        for i in palabras:
+
+            i = i.capitalize()
+
+            if i not in diccionario:
+                significado = raw_input("¿Cómo se escribe '%s' en %s?: " % (i, lengua))
+                significado = significado.lower()
+                significado = significado.capitalize()
+                diccionario[i] = significado
+
+    print "\nDiccionario: \n"
+    for i in diccionario:
+        print i, "\t=", diccionario[i]
+
+main()
