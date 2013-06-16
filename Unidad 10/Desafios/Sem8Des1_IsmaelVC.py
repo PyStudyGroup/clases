678549fea37e23dcffbcfd4e4b6563ae8150383b
Ismael VC <ismael.vc1337@gmail.com>
2013-06-16 10:22:04 -0500

    Actualizado, soporta Winows y Linux y añadido regex.

--- Unidad 10/Desafios/Sem8Des1_IsmaelVC.py
+++ Unidad 10/Desafios/Sem8Des1_IsmaelVC.py
@@ -1,35 +1,98 @@
 #!/usr/bin/env python3
 
+import os
+import re
+import subprocess
 import sys
 
-from subprocess import call
-from textwrap import dedent as dd
 from time import sleep
 
-def validar_cadena(cadena):
+from textwrap import dedent as dd
+
+
+ESPECIFICACION = '''
+Literales de puntos flotantes.
+
+Los literales de puntos flotantes son descritos por las siguientes definiciones lexicas:
+
+numeroflotante    ::=  puntoflotante | exponenteflotante
+puntoflotante     ::=  [parteint] fraccion | parteint "."
+exponenteflotante ::=  (parteint | puntoflotante ) exponente
+parteint          ::=  digito+
+fraccion          ::=  "." digito+
+exponente         ::=  ("e" | "E") ["+" | "-"] digito+
+
+Note que las partes integrales y exponentes siempre son interpretadas usando base 10.
+Por ejemplo, 077e010 es legal, y denota al mismo numero que 77e10. El rango de literales
+de punto flotante permitido depende de la implementacion. Algunos ejemplos de literales
+de puntos flotantes:
+
+3.14    10.    .001    1e100    3.14e-10    0e0
+
+Note que los literales numericos no incluyen un signo; la frase -1 es en realidad una
+expresion compuesta por el operador unario - y el literal 1.
+'''
+
+regex = r'''
+        ^          # Coincide con el inicio de la cadena,
+        [\s]*      # seguido por 0 o mas espacios,
+        ([+-]?)    # seguido por 0 o 1 coincidencia del signo de positivo o negativo,
+        ([\d]*)    # seguido por 0 o mas numeros (parte entera),
+        ([.]?)     # seguido por 0 o 1 punto,
+        ([\d]*)    # seguido por 0 o mas numeros (parte flotante),
+        ([e]?)     # seguido por 0 o 1 coincidencia de exponente,
+        ([-]?)     # seguido por 0 o 1 signo negativo del exponente,
+        ([\d]*)    # seguido por 0 o mas numeros (parte exponente),
+        [\s]*      # seguido por 0 o mas espacios,
+        $          # seguido por el final de la cadena.
+        '''
+
+patron = re.compile(regex, re.I|re.X)
+error = '''"ERROR! La cadena: '{cadena}', NO es una expresion valida, para numeros flotantes.".format_map(vars())'''
+
+def clear():
+    if os.name == 'posix':
+        subprocess.call('clear')
+    else:
+        os.system('cls')
+
+
+def primera_validacion(cadena):
+    if cadena.lower() in ['inf', 'infinity', 'nan']:
+        return cadena
+    coincidencia = patron.search(cadena)
+    if coincidencia:
+        return coincidencia.group()
+    else:
+        print(eval(error))
+
+
+def segunda_validacion(cadena):
     try:
         float(cadena)
     except ValueError:
-        print("ERROR! La cadena: '{cadena}', NO es una expresion valida, para numeros flotantes.".format(**vars()))
+        print('\nHumanProcessorError: Falso positivo en la primera validacion.\n')
+        print(eval(error))
     else:
-        print("La cadena: '{cadena}', es una expresion valida, para numeros flotantes.".format(**vars()))
-  
+        print("\nLa cadena: '{cadena}', es una expresion valida, para numeros flotantes.".format_map(vars()))
+
 
 def main():
-    prompt = chr(10144)
+    # prompt = chr(10144)   # cmd.exe es tan patetico...
     while True:
-        call('clear')
+        clear()
         cadena_in = input(dd('''\
-                             Escriba una expresion para numeros flotantes, 
+                             Escriba una expresion para numeros flotantes,
                              su validez sera evaluada: (S/SALIR, para finalizar)
-                             {prompt} '''.format(**vars())))
+                             => '''.format_map(vars())))
         if cadena_in.lower() in ['s', 'salir']:
             sys.exit()
         else:
-            validar_cadena(cadena_in)
-            sleep(4)
-    
-  
+            prueba = primera_validacion(cadena_in)
+            if prueba:
+                segunda_validacion(prueba)
+            sleep(5)
+
+
 if __name__ == '__main__':
     main()
-
